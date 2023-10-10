779e381f68a34e4c885d8f5019379f7692e98b38
elmaouadda <43704088+elmaouadda@users.noreply.github.com>
2023-10-11 00:57:27 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let calculation = localStorage.getItem("calculation") || "";
+
+      displayResult();
+
+      function updateCalculation(value) {
+        calculation += value;
+        displayResult();
+
+        localStorage.setItem("calculation", calculation);
+      }
+
+      function displayResult() {
+        document.querySelector(".js-display").innerHTML = calculation;
+      }
